e9e0528b4c62401695d27c80ed5aa80f578fdb7d
hridoy123523 <dxridoy081@gmail.com>
2025-09-09 19:52:15 +0600

    Create layout.js
    Signed-off-by: hridoy123523 <dxridoy081@gmail.com>

---
+++ app/app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "moveibajar2.0",
+  description: "Download your favorite movies easily",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
